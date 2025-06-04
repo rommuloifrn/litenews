966f53b5691d6aad6ab5fe4150072c6a6ec2c348
rommuloifrn <roomulo.cavalkant@outlook.com>
2025-06-04 09:42:46 -0300

    wip: start PostRepository

--- app/src/main/java/com/example/litenews/repository/PostRepository.kt
+++ app/src/main/java/com/example/litenews/repository/PostRepository.kt
@@ -1,4 +1,26 @@
 package com.example.litenews.repository
 
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+
+
 class PostRepository {
+
+    // Function that makes the network request, blocking the current thread
+    fun getPosts(): String {
+        val pagina = 1
+        val porPagina = 10
+        val estrategia = "relevant"
+        val url = "https://www.tabnews.com.br/api/v1/contents?page=$pagina&per_page=$porPagina&strategy=$estrategia"
+        val client = OkHttpClient()
+
+        val request: Request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).execute().use { response: Response ->
+            return response.body!!.string()
+        }
+    }
 }
